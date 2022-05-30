78d112d49a2f4038de4a3645458854f33b1a9bbe
AyushParmar <ayushamanparmar@gmail.com>
2022-05-30 22:44:23 +0530

    UI In Game Scene

--- GameJam_Unity/Assets/Scripts/UI Scripts/UIManager.cs
+++ GameJam_Unity/Assets/Scripts/UI Scripts/UIManager.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] Image sprintBarImage;
-    [SerializeField] Image healthBarImage;
-    [SerializeField] Image manaBarImage;
     [SerializeField] static bool GameIsPaused = false;
     [SerializeField] GameObject pauseMenu;
 
@@ -57,42 +53,5 @@ public class UIManager : MonoBehaviour
     public void BackToMainMenu()
     {
         SceneManager.LoadScene(0);
-    }
-
-    
-
-    //Sprint Bar
-    public void UpdateSprintBar(float sprintFrac)
-    {
-        sprintBarImage.fillAmount = Mathf.Clamp(sprintFrac , 0, 1f);
-    }
-
-    //Mana Bar
-    public void UpdateManaBar(float manaFrac)
-    {
-        manaBarImage.fillAmount = Mathf.Clamp(manaFrac, 0, 1f);
-    }
-
-    //Health Bra
-    public void SetHealthBarColor(Color healthColor)
-    {
-        healthBarImage.color = healthColor;
-    }
-
-    public void UpdateHealthBar(float healthBar)
-    {
-        healthBarImage.fillAmount = Mathf.Clamp(healthBar, 0, 1f);
-        if(healthBarImage.fillAmount < 0.2f)
-        {
-            SetHealthBarColor(Color.red);
-        }
-        else if(healthBarImage.fillAmount < 0.4f)
-        {
-            SetHealthBarColor(Color.yellow);
-        }
-        else
-        {
-            SetHealthBarColor(Color.green);
-        }
     }
 }
